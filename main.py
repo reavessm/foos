563b4d9f8edd4e0a61b46ffd51ead018f0526bf0
itoth43 <itoth@g.clemson.edu>
2019-05-17 18:27:00 -0400

    UI first draft

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from gpiozero import LED
 from time import sleep
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.clock import Clock
 
-led = LED(17)
+class FoosGame(Widget):
+    score = NumericProperty(0)
+    team1 = ObjectProperty(None)
+    team2 = ObjectProperty(None)
+    
+    def update(self, dt):
+        self.team1.score += 1
+        self.team2.score += 1
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+class FoosApp(App):
+    
+    def build(self):
+        game = FoosGame()
+        Clock.schedule_interval(game.update, 1.0/60.0)
+        return game
+
+if __name__ == '__main__':
+    FoosApp().run()
+
+
