18f8f730ec357a01795000385cfeb2cb54a7c8d1
Stephen Reaves <reaves735@gmail.com>
2019-05-21 13:48:16 -0400

    :rocket: Added video player

--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ##cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=x264{keyint=60,idrint=2},vb=400,width=368,heigh=208,acodec=mp4a,ab=32,channels=2,fps=25}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=asf,dst=:8082/}}' --no-sout-audio
-cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=x264{keyint=60,idrint=2},vb=400,width=368,heigh=208,acodec=mp4a,ab=32,channels=2,fps=15}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=ffmpeg{mux=flv},dst=:8082/}}' --no-sout-audio
+cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=x264{keyint=60,idrint=2},vb=400,width=368,heigh=208,acodec=mp4a,ab=32,channels=2,fps=25}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=ffmpeg{mux=flv},dst=:8082/}}' --no-sout-audio
 #cvlc v4l2:// :v4l2-vdev="/dev/video0" --sout '#transcode{vcodec=x264{keyint=60,idrint=2},vb=400,width=1024,height=576,acodec=mp4a,ab=32,channels=2,fps=15}:duplicate{dst=std{access=http{mime=video/x-ms-wmv},mux=ffmpeg{mux=flv},dst=:8082/}}' --no-sout-audio
--- video.py
+++ video.py
@@ -1,17 +1,26 @@
 import cv2
+import time
 
 cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+vc = cv2.VideoCapture("http://localhost:8082")
 
 if vc.isOpened():
     rval, frame = vc.read()
 else:
     rval = False
 
+counter = 0
+
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
+    time.sleep(0.2)
+    if counter >= 150:
+        break
+    else:
+        counter += 1
+        print(counter)
     if key == 27: # Esc
         break
 cv2.destroyWindow("preview")
